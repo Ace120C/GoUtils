51e0e7c398295b9d064d8b962792918197936097
Ace <ace@localhost.localdomain>
2025-08-10 15:07:52 +0100

    fixed touch and mkdir

--- Goutils/mkdir/mkdir.go
+++ Goutils/mkdir/mkdir.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 var DirName string
 
 func main()  {
 	Args := os.Args[1:]
-	if len(os.Args) > 0 {
-		DirName = strings.Join(Args, " ")
-	}
 
 	for _, DirName := range Args {
 		err := os.Mkdir(DirName, 0750)
--- Goutils/touch/touch.go
+++ Goutils/touch/touch.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 var FileName string
 
 func main()  {
 	Args := os.Args[1:]
-	if len(os.Args) > 0 {
-		FileName = strings.Join(Args, " ")
-	}
 
 	for _, FileName := range Args {
 		f, err := os.Create(FileName)
